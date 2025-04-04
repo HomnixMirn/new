9e65a6a02e98b2d337ceecf2af743d9f8b4cc22e
desssty <dimonmikryukov@gmail.com>
2025-04-05 01:05:17 +0400

    поменял шапку

--- front/src/app/components/header/page.tsx
+++ front/src/app/components/header/page.tsx
@@ -6,29 +6,25 @@ import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
 import LoginForm from "@components/header/login/page";
 import { useUser } from "@/hooks/user-context";
-
 export default function Header() {
   const pathname = usePathname();
   const [isLoginOpen, setIsLoginOpen] = useState(false);
   const { user, fetchUser } = useUser();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
   useEffect(() => {
     if (user) {
+      console.log("Текущий пользователь:", user);
       setIsLoggedIn(true);
     }
   }, [user]);
-
   const handleLoginSuccess = () => {
     setIsLoggedIn(true);
     setIsLoginOpen(false);
   };
-
   const handleLogout = () => {
     setIsLoggedIn(false);
     localStorage.removeItem("token");
   };
-
   return (
     <>
       <header className="bg-white w-full h-[68px] content-center">
@@ -38,48 +34,46 @@ export default function Header() {
               SKILLSWAP
             </span>
           </Link>
-
           <div className="flex w-[35%] content-evenly gap-[20px] flex-row items-center justify-between">
             <Link
-              className="flex flex-col content-center items-center text-[#898989] hover:text-[#2E4C59]"
+              className="flex flex-col content-center items-center text-[#898989] hover:text-blue-500"
               href="/"
             >
-              Home
+              Главная
               {pathname === "/" && (
-                <div className="relative w-[150px] h-[1px] bg-black hover:bg-[#2E4C59]"></div>
+                <div className="relative w-[200%] h-0.5 bg-blue-500"></div>
               )}
             </Link>
             <Link
-              className="flex flex-col items-center text-[#898989] hover:text-[#2E4C59] hover:underline after"
-              href="Hotels_for_saleome"
+              className="flex flex-col items-center text-[#898989] hover:text-blue-500 "
+              href="/catalog"
             >
-              Hotels for sale
-              {pathname === "/Hotels_for_saleome" && (
-                <div className="relative w-[150px] h-[1px] bg-black hover:bg-[#2E4C59]"></div>
+              Каталог
+              {pathname === "/catalog" && (
+                <div className="relative w-[200%] h-0.5 bg-blue-500"></div>
               )}
             </Link>
             <Link
-              className="flex flex-col items-center text-[#898989] hover:text-[#2E4C59] hover:underline"
-              href="News"
+              className="flex flex-col items-center text-[#898989] hover:text-blue-500 "
+              href="/map"
             >
-              News
-              {pathname === "/News" && (
-                <div className="relative w-[150px] h-[1px] bg-black hover:bg-[#2E4C59]"></div>
+              Карта
+              {pathname === "/map" && (
+                <div className="relative w-[200%] h-0.5 bg-blue-500"></div>
               )}
             </Link>
             {isLoggedIn && (
               <Link
-                className="flex flex-col items-center text-[#898989] hover:text-[#2E4C59] hover:underline"
-                href="profile"
+                className="flex flex-col items-center text-[#898989] hover:text-blue-500"
+                href="/profile"
               >
                 Профиль
                 {pathname === "/profile" && (
-                  <div className="relative w-[150px] h-[1px] bg-black hover:bg-[#2E4C59]"></div>
+                  <div className="relative w-[200%] h-0.5 bg-blue-500"></div>
                 )}
               </Link>
             )}
           </div>
-
           <div className="ontent-center justify-around flex ">
             {isLoggedIn ? (
               <SolidButton
@@ -97,7 +91,6 @@ export default function Header() {
           </div>
         </div>
       </header>
-
       {isLoginOpen && (
         <LoginForm
           onClose={() => setIsLoginOpen(false)}
