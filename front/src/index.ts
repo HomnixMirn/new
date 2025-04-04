ce587ff4a6c15ce1b88ef56bebe237560cb33291
Shadow7x <loginvlad1000@gmail.com>
2025-04-04 12:09:14 +0400

    fix and axi and user-context

---
+++ front/src/index.ts
@@ -0,0 +1,4 @@
+// export const API_URL = "http://localhost:80/api/";
+// export const MEDIA_URL = "http://localhost:80";
+export const API_URL = "http://localhost:8000/api/";
+export const MEDIA_URL = "http://localhost:8000";
