954efa9b82f99f1a3c552dc15968134c03b85624
Shadow7x <loginvlad1000@gmail.com>
2025-04-09 23:36:49 +0300

    для даньки

--- backend/back/core/models/models.py
+++ backend/back/core/models/models.py
@@ -14,54 +14,54 @@ class Profile(models.Model):
     
     
 class Office(models.Model):
-    name = models.CharField(max_length=50)
-    locationType = models.CharField(max_length=50)
-    title = models.CharField(max_length=50, null=True, blank=True)
+    name = models.CharField(max_length=150)
+    locationType = models.CharField(max_length=150)
+    title = models.CharField(max_length=150, null=True, blank=True)
     latitude = models.FloatField(null=True, blank=True)
     longitude = models.FloatField(null=True, blank=True)
-    city = models.CharField(max_length=50, null=True, blank=True)
-    cityId = models.CharField(max_length=50, null=True, blank=True)
-    address = models.CharField(max_length=50, null=True, blank=True)
-    subwayStation = models.CharField(max_length=50, null=True, blank=True)
-    landmarks = models.CharField(max_length=50, null=True, blank=True)
+    city = models.CharField(max_length=150, null=True, blank=True)
+    cityId = models.CharField(max_length=150, null=True, blank=True)
+    address = models.CharField(max_length=150, null=True, blank=True)
+    subwayStation = models.CharField(max_length=150, null=True, blank=True)
+    landmarks = models.CharField(max_length=150, null=True, blank=True)
     daySchedules = models.ManyToManyField('daySchedule')
     services = models.ManyToManyField('service')
-    salePointId = models.CharField(max_length=50, null=True, blank=True)
-    phoneNumberOrdering = models.CharField(max_length=50, null=True, blank=True)
-    mnpOrdering = models.CharField(max_length=50, null=True, blank=True)
-    deviceOrdering = models.CharField(max_length=50, null=True, blank=True)
-    esimOrdering = models.CharField(max_length=50, null=True, blank=True)
-    b2bShopAvailability = models.CharField(max_length=50, null=True, blank=True)
-    tele2Store = models.CharField(max_length=50, null=True, blank=True)
-    amountThreshold = models.CharField(max_length=50, null=True, blank=True)
+    salePointId = models.CharField(max_length=150, null=True, blank=True)
+    phoneNumberOrdering = models.CharField(max_length=150, null=True, blank=True)
+    mnpOrdering = models.CharField(max_length=150, null=True, blank=True)
+    deviceOrdering = models.CharField(max_length=150, null=True, blank=True)
+    esimOrdering = models.CharField(max_length=150, null=True, blank=True)
+    b2bShopAvailability = models.CharField(max_length=150, null=True, blank=True)
+    tele2Store = models.CharField(max_length=150, null=True, blank=True)
+    amountThreshold = models.CharField(max_length=150, null=True, blank=True)
     sites = models.ManyToManyField('site')
-    externalLocationId = models.CharField(max_length=50, null=True, blank=True)
-    email = models.CharField(max_length=50, null=True, blank=True)
-    showcaseId = models.CharField(max_length=50, null=True, blank=True)
+    externalLocationId = models.CharField(max_length=150, null=True, blank=True)
+    email = models.CharField(max_length=150, null=True, blank=True)
+    showcaseId = models.CharField(max_length=150, null=True, blank=True)
     comments = models.ManyToManyField('comments')
     
     def __str__(self):
         return self.name
     
 class daySchedule(models.Model):
-    day = models.CharField(max_length=50)
-    openTime = models.CharField(max_length=50, null=True, blank=True)
-    closeTime = models.CharField(max_length=50, null=True, blank=True)
+    day = models.CharField(max_length=150)
+    openTime = models.CharField(max_length=150, null=True, blank=True)
+    closeTime = models.CharField(max_length=150, null=True, blank=True)
     dayOff = models.BooleanField()
     
     
 class service(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=150)
     weight = models.IntegerField()
     
 class site(models.Model):
-    name = models.CharField(max_length=50)
-    contextRoot = models.CharField(max_length=50)
-    productionUrl = models.CharField(max_length=50)
+    name = models.CharField(max_length=150)
+    contextRoot = models.CharField(max_length=150)
+    productionUrl = models.CharField(max_length=150)
     lkEnabled = models.BooleanField()
     shopEnabled = models.BooleanField()
     shopDomainPilot = models.BooleanField()
-    defaultLanguage = models.CharField(max_length=50)
+    defaultLanguage = models.CharField(max_length=150)
         
         
 class comments(models.Model):
