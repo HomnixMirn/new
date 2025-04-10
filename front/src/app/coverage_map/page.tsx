07eb20ed700ecfb87832aabe13c4add64559a779
Shadow7x <loginvlad1000@gmail.com>
2025-04-10 03:15:16 +0300

    checkbox

--- front/src/app/coverage_map/page.tsx
+++ front/src/app/coverage_map/page.tsx
@@ -506,6 +506,7 @@ export default function CoverageMap({
             <Services
               services={services}
               onServiceToggle={servicesUpdateHandle}
+              setServices={setServices}
             />
           ) : (
             <>
